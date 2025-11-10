3a41e5903338a3812f76abb346899baa34cede4a
Arthur-Fortunato <arthur.r.t.fortunato@gmail.com>
2025-11-10 16:06:05 -0300

    feat: criação do repositório

---
+++ src/main/java/org/example/restapi/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package org.example.restapi.repository;
+
+import org.example.restapi.entity.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
