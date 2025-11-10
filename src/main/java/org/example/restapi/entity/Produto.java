2bac115cae2733acea5b4a7557f6c552bbe5ce14
Arthur-Fortunato <arthur.r.t.fortunato@gmail.com>
2025-11-10 16:01:20 -0300

    feat: criação da entidade Produto

---
+++ src/main/java/org/example/restapi/entity/Produto.java
@@ -0,0 +1,16 @@
+package org.example.restapi.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "produtos")
+@Data
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private double preco;
+    private double quantidade;
+}
